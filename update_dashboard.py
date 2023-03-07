045c5a58b201dcc7ffc9dfd2a1beeeab7ed6076a
ISzafarowicz <igor.szafarowicz@gmail.com>
2023-03-07 08:33:50 +0100

    added csv gameweek data

--- update_dashboard.py
+++ update_dashboard.py
@@ -3,5 +3,5 @@ from Scraping_Module.scraping_fpl_api import FplScraper
 
 scraper = FplScraper()
 curr_gw = scraper.load_gameweek()
-curr_gw.to_excel('Dashboard_Data/current_gameweek_data.xlsx', index=False)
+curr_gw.to_csv('Dashboard_Data/current_gameweek_data.csv', index=False)
 print('Current Gameweek Data Updated')
