0c2a66d103188a1ad589e9b2295733a1c79b8727
ISzafarowicz <igor.szafarowicz@gmail.com>
2023-03-07 09:09:06 +0100

    dashboard update

--- update_dashboard.py
+++ update_dashboard.py
@@ -2,6 +2,6 @@ import pandas as pd
 from Scraping_Module.scraping_fpl_api import FplScraper
 
 scraper = FplScraper()
-curr_gw = scraper.load_gameweek(GW = 25)
+curr_gw = scraper.load_gameweek()
 curr_gw.to_csv('Dashboard_Data/current_gameweek_data.csv', index=False)
 print('Current Gameweek Data Updated')
